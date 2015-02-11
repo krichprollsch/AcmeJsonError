04222de59273e593db09d455018729637e17bd28
Pierre Tachoire <ptachoire@itnetwork.fr>
2015-02-11 20:21:19 +0100

    add sample actions to AppBundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,39 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/app/example", name="homepage")
+     * @Route("/", name="homepage")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/exception")
+     */
+    public function exceptionAction()
+    {
+        throw new \RuntimeException('This is a runtime exception');
+    }
+
+    /**
+     * @Route("/error")
+     */
+    public function errorAction()
+    {
+        trigger_error('This is an error');
+    }
+
+    /**
+     * @Route("/notfound")
+     */
+    public function notfoundAction()
+    {
+        throw new NotFoundHttpException('This is not found');
     }
 }
